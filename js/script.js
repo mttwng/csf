fd09bff50e29da1d004dda76f58bb69976ec6df3
Matthew Wong <mtthwwng@gmail.com>
2015-01-13 00:13:37 -0800

    First Sync

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+
+   // if navbar is not collapsed
+   if ($window.width() >= 768) { 
+ 
+     $('section[data-type="background"]').each(function(){
+       // declare the variable to affect the defined data-type
+       var $scroll = $(this);
+                       
+        $(window).scroll(function() {
+          // HTML5 proves useful for helping with creating JS functions!
+          // also, negative value because we're scrolling upwards                            
+          var yPos = -($window.scrollTop() / $scroll.data('speed'));
+           
+          // background position
+          var coords = '50% '+ yPos + 'px';
+   
+          // move the background
+          $scroll.css({ backgroundPosition: coords });   
+        }); // end window scroll
+     });  // end section function
+    }
+
+    $(".person_info").hover(
+    //on mouseover
+    function() {
+      $(this).animate({
+        paddingTop: '+=10',
+        height: '+=140', //adds 250px
+        marginTop: '-=170'
+        }, 'normal' //sets animation speed to slow
+      );
+    },
+    //on mouseout
+    function() {
+      $(this).animate({
+        paddingTop: '-=10',
+        height: '-=140px', //substracts 250px
+        marginTop: '+=170',
+        zIndex: '0'
+        }, 'normal'
+      );
+    }
+  );
+}); // close out script
+
+/* Create HTML5 element for IE */
+document.createElement("section");
+
